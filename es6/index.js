73dcc9ca1a6cea51796bfe8003ffd59468392078
Andrea Parodi <a.parodi@inet2.it>
2015-08-26 16:12:34 +0200

    first release

--- es6/index.js
+++ es6/index.js
@@ -1,3 +1,37 @@
-export default async function chute() {
-  return Promise.resolve(42);
+import fs from 'vinyl-fs';
+import multipipe from 'multipipe';
+import through2 from 'through2';
+import { EventEmitter } from 'events';
+
+export default function chute(...transforms) {
+  let emitError = null;
+  const result = Object.assign(new EventEmitter(), {
+
+    push(...globs) {
+      const files = fs.src(...globs);
+      files.on('error', emitError);
+      files.pipe(this.pipe);
+      return this;
+    },
+
+    save(folder) {
+      this.targetStream = this.pipe.pipe(fs.dest(folder));
+      return this;
+    }
+  });
+  const transformStreams = transforms
+    .map(fn => (chunk, enc, cb) => {
+      try {
+        return fn(chunk, enc, cb);
+      } catch (err) {
+        cb(err);
+      }
+    })
+    .map(through2.obj);
+
+  result.pipe = multipipe(...transformStreams);
+  emitError = result.emit.bind(result, 'error');
+  result.pipe.on('error', emitError);
+
+  return result;
 }
--- test/index_test.js
+++ test/index_test.js
@@ -4,11 +4,110 @@ if (process.env.TEST_RELEASE) {
 }
 
 const chute = require(moduleRoot);
+import { readFileSync, unlinkSync, existsSync } from 'fs';
 
-describe('chute', () => {
-  it('works', async () => {
-    const result = await chute();
-    result.should.be.equal(42);
+describe('chute', function() {
+  this.timeout(5000);
+
+  it('is a function', () => {
+    chute.should.be.a('function');
+  });
+
+  describe('that', () => {
+    let chuteObj;
+
+    before( () => {
+      chuteObj = chute(() => {});
+    });
+
+    it('create a chute object', () => {
+      chuteObj.should.be.a('object');
+    });
+
+    it('catch sync errors of transforms in pipeline', done => {
+      const c = chute(() => {
+        throw new Error('an error');
+      });
+      c.on('error', err => {
+        err.message.should.be.equal('an error');
+        done();
+      });
+      c.push(`${__dirname}/fixtures/simple.js`);
+    });
+
+    it('catch async errors of transforms in pipeline', done => {
+      const c = chute((file, enc, cb) => {
+        cb(new Error('an error'));
+      });
+      c.on('error', err => {
+        err.message.should.be.equal('an error');
+        done();
+      });
+      c.push(`${__dirname}/fixtures/simple.js`);
+    });
+
+    describe('with', () => {
+      it('a pipe stream', () => {
+        chuteObj.pipe.on.should.be.a('function');
+      });
+
+      describe('push', () => {
+        it('is method', () => {
+          chuteObj.push.should.be.a('function');
+        });
+
+        it('pipes vinyl files into the pipeline', done => {
+          chute(file => {
+            file.constructor.name.should.be.equal('File');
+            done();
+          }).push(`${__dirname}/fixtures/simple.js`);
+        });
+
+        it('return chute instance for chaining', () => {
+          const c = chute(() => {});
+          const c2 = c.push(`${__dirname}/fixtures/simple.js`);
+          c2.should.be.equal(c);
+        });
+
+        it('vinyl files into the pipeline', done => {
+          chute(file => {
+            file.constructor.name.should.be.equal('File');
+            file.contents.should.be.instanceOf(Buffer);
+            done();
+          }).push(`${__dirname}/fixtures/simple.js`);
+        });
+      });
+
+      describe('save', () => {
+        it('is method', () => {
+          chuteObj.save.should.be.a('function');
+        });
+
+        it('when given a string save all files to destination', done => {
+          const targetPath = `${__dirname}/output/simple.js`;
+          if (existsSync(targetPath)) {
+            unlinkSync(targetPath);
+          }
+
+          const c = chute((file, env, cb) => {
+            file.contents = new Buffer(file.contents.toString('utf-8').toUpperCase());
+            cb(null, file);
+          });
+          c.on('error', done);
+
+          c.save(`${__dirname}/output`);
+          c.push(`${__dirname}/fixtures/simple.js`);
+
+          c.targetStream.on('error', done);
+          c.targetStream.on('finish', () => {
+            const res = readFileSync(targetPath, 'utf-8');
+            res.should.be.equal('CONST RESPONSE=42;\n');
+            unlinkSync(targetPath);
+            done();
+          });
+        });
+      });
+    });
   });
 });
 
