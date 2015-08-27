eef09e810cdc5b61a5b60f5a6b9e61cd9590fb67
Andrea Parodi <a.parodi@inet2.it>
2015-08-27 17:50:20 +0200

    reimplemented as class

--- es6/index.js
+++ es6/index.js
@@ -4,52 +4,53 @@ import through2 from 'through2';
 import { EventEmitter } from 'events';
 import ss from 'stream-stream';
 
+const guardSyncErrors = fn => (chunk, enc, cb) => {
+  try {
+    return fn(chunk, enc, cb);
+  } catch (err) {
+    cb(err);
+  }
+};
+
+class Chute extends EventEmitter {
+  constructor(transforms) {
+    super();
+
+    this.inputStream = ss({ objectMode: true });
+
+    const transformStreams = transforms
+      .map(guardSyncErrors)
+      .map(through2.obj);
+
+    this.pipe = multipipe(...transformStreams);
+
+    this.inputStream.pipe(this.pipe);
+
+    this.pipe.on('error', err => this.emit('error', err));
+  }
+
+  push(...globs) {
+    const files = fs.src(...globs);
+    files.on('error', err => this.emit('error', err));
+    this.inputStream.write(files);
+
+    return this;
+  }
+
+  end() {
+    setTimeout( () => {
+      this.pipe.on('finish', () => this.emit('finish'));
+      this.inputStream.end();
+    }, 10);
+  }
+
+  save(folder) {
+    const targetStream = this.pipe.pipe(fs.dest(folder));
+    targetStream.on('finish', () => this.emit('saved'));
+    return this;
+  }
+}
+
 export default function chute(...transforms) {
-  let emitError = null;
-  let emitFinish = null;
-
-
-  const result = Object.assign(new EventEmitter(), {
-    inputStream: ss({ objectMode: true }),
-
-    push(...globs) {
-      const files = fs.src(...globs);
-      files.on('error', emitError);
-      this.inputStream.write(files);
-
-      return this;
-    },
-
-    end() {
-      setTimeout( () => {
-        this.pipe.on('finish', emitFinish);
-        this.inputStream.end();
-      }, 10);
-    },
-
-    save(folder) {
-      const targetStream = this.pipe.pipe(fs.dest(folder));
-      targetStream.on('finish', () => this.emit('saved'));
-      return this;
-    }
-
-  });
-  const transformStreams = transforms
-    .map(fn => (chunk, enc, cb) => {
-      try {
-        return fn(chunk, enc, cb);
-      } catch (err) {
-        cb(err);
-      }
-    })
-    .map(through2.obj);
-
-  result.pipe = multipipe(...transformStreams);
-  result.inputStream.pipe(result.pipe);
-
-  emitError = result.emit.bind(result, 'error');
-  emitFinish = result.emit.bind(result, 'finish');
-  result.pipe.on('error', emitError);
-
-  return result;
+  return new Chute(transforms);
 }
